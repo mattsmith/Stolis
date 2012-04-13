be95d72e19e5916fab40045c8f5051c16ecee2d1
Matt Smith <mattr.smith@gmail.com>
2012-04-12 19:53:18 -0700

    rails new cod

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stolis::Application.config.secret_token = 'd21581f247508cc0c1b955eb772dac33362bbde77c27e6db3a176096fd22c0d40202066e5df1339b846ed255b52d48c5a97b32349fc7b55cb20a2b69baf50f16'
