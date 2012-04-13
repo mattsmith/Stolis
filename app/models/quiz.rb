ad29ce143b81b00a3723cf8c1890d36cab76bd09
Matt Smith <mattr.smith@gmail.com>
2012-04-13 15:37:50 -0700

    cleaned up small requests, added style for active buttons, added padding to radio buttons and validation for email

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,5 +3,7 @@ class Quiz < ActiveRecord::Base
 
   attr_accessible :cocktail, :email, :answers_attributes
 
+  validates :email, format: {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/}, on: :update
+
   accepts_nested_attributes_for :answers
 end
